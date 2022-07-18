8a91a1a845c31d2fedc0f09438d0663df5aa3063
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-07-17 23:27:04 -0400

    set up portfolio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import coverImage from '../../assets/cover/cover-image.jpg';
+
 function About() {
-  return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
-      <div className="my-2">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus. Proin aliquam justo nec diam vulputate vestibulum.
-          Aenean sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi tristique
-          justo vel turpis sollicitudin, et tristique velit convallis. In hac habitasse platea
-          dictumst. Phasellus mattis nunc sed orci consequat laoreet. Praesent id nisl nibh.
-          Curabitur imperdiet ultricies mollis. In hac habitasse platea dictumst.
-        </p>
-      </div>
-    </section>
-  );
+	return (
+		<section>
+			<div className="center" id="about">
+				<h1 className="page-header">About Me</h1>
+			</div>
+			<div className="center">
+				<img
+					src={require('../../assets/images/family.jpeg')}
+					alt="about-me"
+					className="photo"
+				/>
+			</div>
+			<div>
+				<p>
+					I am a full stack web developer looking to build a more
+					intuitive user experience on the web. I recently earned
+					a certificate in full stack development from the
+					Columbia University bootcamp, with newly developed
+					skills in JavaScript, CSS, React.js, and responsive web
+					design. I am known as an innovative problem solver
+					passionate about developing apps, with a focus on
+					mobile-first design and development. With each project,
+					my aim is to best engage my audience for an impactful
+					user experience.
+				</p>
+				<p>
+					I applied aspects of UX and agile development in a
+					recent project. I worked on a team of five to develop 
+					a single-page MERN app that allows users to post blogs and to track other posts from other users.
+          I am excited to leverage my skills as part
+					of a fast-paced, quality-driven team to build better
+					experiences on the web.
+				</p>
+			</div>
+		</section>
+	);
 }
 
 export default About;
---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import Project from '../Project';
+
+function Portfolio() {
+	const projects = [
+		{
+			name: 'Run Buddy',
+			description:
+				'This project was the first of many that was an introduction to the front-end technologies of HTML and CSS. A website that offers fitness training services.',
+			image: 'run-buddy.jpg',
+			technologies: [
+				'HTML/CSS'
+			],
+			github: 'https://github.com/brettshackett/run-buddy',
+			deployed: 'https://brettshackett.github.io/run-buddy/',
+		},
+		{
+			name: 'Pay Up',
+			description:
+				'Pay Up allows a user to enter a bill amount that needs to be split amongst a group of individuals and it will return the amount evenly split between a user entered amount of individuals.',
+			image: 'payup.png',
+			technologies: [
+				'HTML/CSS',
+				'JavaScript',
+				'Node.js',
+				'Handlebars.js',
+				'Express & Sequelize',
+				'Heroku & AWS S3',
+			],
+			github: 'https://github.com/OwaisIslam/auto-tracker',
+			deployed: 'https://team-3-auto-tracker.herokuapp.com/',
+		},
+		{
+			name: 'Photo Port',
+			description:
+				'My first attempt using React to create a website that displays pictures for a photgrapher. This was apart of the bootcamp curriculum but was great practice for React.',
+			image: 'photo-port.jpg',
+			technologies: ['HTML/CSS', 'JavaScript', 'React.js'],
+			github: 'https://github.com/brettshackett/Photo-Port',
+		},
+		{
+			name: 'Budget Tracker',
+			description:
+				'Budget Tracker is an Application that uses a noSQL database and offline persistance for a great user experience for managing their finances and budgeting no matter where they are.',
+			image: 'budget-tracker-image.png',
+			technologies: [
+				'JavaScript',
+				'MongoDB',
+				'IndexedDB & Service Workers',
+				'Node.js',
+				'Express',
+			],
+			github: 'https://github.com/brettshackett/budget-tracker',
+			deployed: 'https://cu-budget-tracker.herokuapp.com/',
+		},
+		{
+			name: 'Weather Dashboard',
+			description:
+				'A weather dashboard that shows current weather and five-day weather forecast for any city.',
+			image: 'weather-dashboard-image.png',
+			technologies: ['HTML/CSS', 'JavaScript'],
+			github: 'https://github.com/OwaisIslam/weather-dashboard',
+			deployed: 'https://owaisislam.github.io/weather-dashboard/',
+		},
+		{
+			name: 'Password Generator',
+			description:
+				'This is an application that allows you to generate a password from 8 to 128 characters based on criteria you select.',
+			image: 'password-generator-image.png',
+			technologies: ['HTML/CSS', 'JavaScript'],
+			github: 'https://github.com/brettshackett/Random-Password-Generator',
+			deployed: 'https://brettshackett.github.io/Random-Password-Generator/',
+		},
+		{
+			name: 'Note Taker',
+			description:
+				'This application allows the user to enter notes and as they save, they appear in the sidebar on the left. Notes can be deleted by clicking the trash can next to the corresponding note.',
+			image: 'note-taker-image.png',
+			technologies: ['HTML/CSS', 'JavaScript', 'Express.js'],
+			github: 'https://github.com/OwaisIslam/note-taker',
+			deployed: 'https://note-taker-owais.herokuapp.com/',
+		},
+	];
+
+	return (
+		<section>
+			<div className="center">
+				<h1 className="page-header">My Portfolio</h1>
+			</div>
+			<div>
+				<ul className="flex-row mobile-row">
+					<li className="padding">
+						<Project projects={projects[0]}></Project>
+					</li>
+					<li className="padding">
+						<Project projects={projects[1]}></Project>
+					</li>
+				</ul>
+				<ul className="flex-row mobile-row">
+					<li className="padding">
+						<Project projects={projects[2]}></Project>
+					</li>
+					<li className="padding">
+						<Project projects={projects[3]}></Project>
+					</li>
+				</ul>
+				<ul className="flex-row mobile-row">
+					<li className="padding">
+						<Project projects={projects[4]}></Project>
+					</li>
+					<li className="padding">
+						<Project projects={projects[5]}></Project>
+					</li>
+				</ul>
+			</div>
+		</section>
+	);
+}
+
+export default Portfolio;
