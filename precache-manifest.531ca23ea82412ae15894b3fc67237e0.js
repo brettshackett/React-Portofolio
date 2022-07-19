5b7503f678f60205a85835ee7fa38ab1dc8607b4
brettshackett <63305041+brettshackett@users.noreply.github.com>
2022-07-18 21:04:08 -0400

    Updates

---
+++ precache-manifest.531ca23ea82412ae15894b3fc67237e0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b741721c18623e4cb6e764d6ab968e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "d962b965f0dc0553c664",
+    "url": "/react-portfolio/static/css/main.61401fff.chunk.css"
+  },
+  {
+    "revision": "57f2aa1f7ca1aeb04574",
+    "url": "/react-portfolio/static/js/2.1689a274.chunk.js"
+  },
+  {
+    "revision": "c9e0c53042442321331287fb1614b34d",
+    "url": "/react-portfolio/static/js/2.1689a274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82eabb2f3b25c58eab5d",
+    "url": "/react-portfolio/static/js/3.7f464f17.chunk.js"
+  },
+  {
+    "revision": "d962b965f0dc0553c664",
+    "url": "/react-portfolio/static/js/main.fa182438.chunk.js"
+  },
+  {
+    "revision": "d5af8a75861976dc893e",
+    "url": "/react-portfolio/static/js/runtime-main.39de6cfe.js"
+  },
+  {
+    "revision": "a6049773651fd7299e3eccab09d771cd",
+    "url": "/react-portfolio/static/media/Resume.docx.a6049773.pdf"
+  },
+  {
+    "revision": "09e6ae3da5daad49c278e9ff4a75743d",
+    "url": "/react-portfolio/static/media/budget-tracker-image.09e6ae3d.png"
+  },
+  {
+    "revision": "e6dc723e926645793441b6b66108c76b",
+    "url": "/react-portfolio/static/media/family.e6dc723e.jpeg"
+  },
+  {
+    "revision": "0e54790e036d3e97597027712ffaab9b",
+    "url": "/react-portfolio/static/media/password-generator-image.0e54790e.png"
+  },
+  {
+    "revision": "117551dd902ded743473fa88122bd1ad",
+    "url": "/react-portfolio/static/media/payup.117551dd.png"
+  },
+  {
+    "revision": "0a0ea3cf7396a7d6a66c93bf75be870e",
+    "url": "/react-portfolio/static/media/photo-port.0a0ea3cf.jpg"
+  },
+  {
+    "revision": "15f3dc3172ec24814a4194e445b255b7",
+    "url": "/react-portfolio/static/media/run-buddy.15f3dc31.jpg"
+  },
+  {
+    "revision": "5db07b5ca4b360c2f9058145e3070ff4",
+    "url": "/react-portfolio/static/media/weather-dashboard-image.5db07b5c.png"
+  }
+]);
